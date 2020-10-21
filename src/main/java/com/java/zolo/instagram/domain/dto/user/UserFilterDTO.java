ec88224127f5e084a73da5ff8e3364af4ba10dbc
zolostays <zolostays@zolostayss-MacBook-Pro.local>
2020-10-21 16:28:37 +0530

    Fetch User from Id Unit Test

--- src/main/java/com/java/zolo/instagram/domain/dto/user/UserFilterDTO.java
+++ src/main/java/com/java/zolo/instagram/domain/dto/user/UserFilterDTO.java
@@ -3,10 +3,12 @@ package com.java.zolo.instagram.domain.dto.user;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.experimental.Accessors;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Accessors(chain = true)
 public class UserFilterDTO {
     private String userName;
     private String emailId;
--- src/test/java/com/java/zolo/instagram/services/UserServiceUnitTests.java
+++ src/test/java/com/java/zolo/instagram/services/UserServiceUnitTests.java
@@ -1,6 +1,7 @@
 package com.java.zolo.instagram.services;
 
 import com.java.zolo.instagram.domain.dto.user.UserDTO;
+import com.java.zolo.instagram.domain.model.User;
 import com.java.zolo.instagram.repository.UsersRepository;
 import com.java.zolo.instagram.service.user.UserServiceImpl;
 import org.junit.Test;
@@ -12,6 +13,9 @@ import org.mockito.junit.MockitoJUnitRunner;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.modelmapper.ModelMapper;
 import org.modelmapper.convention.MatchingStrategies;
+import static org.mockito.BDDMockito.*;
+
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -29,19 +33,43 @@ public class UserServiceUnitTests {
 
     @Test
     public void createUser_success() {
+        // arrange
         UserDTO userDTO = new UserDTO().setUserName("test_user")
+                .setEmailId("test@gmail.com")
+                .setPassword("abcxyz")
+                .setProfileName("Test User");
+        modelMapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
+        User user = modelMapper.map(userDTO, User.class);
+        String expectedUserName = "test_user";
+        given(usersRepository.save(user)).willReturn(user); // mocking repository's behavior
+
+        // act
+        UserDTO actualUserDTO = userService.createUser(userDTO);
+
+        // assert
+        assertEquals(expectedUserName, actualUserDTO.getUserName());
+        verify(usersRepository).save(any(User.class));
+    }
+
+    @Test
+    public void fetchUserFromId_success() {
+        User user = new User().setId(12345l).setUserName("test_user")
                 .setEmailId("test@gmail.com")
                 .setPassword("abcxyz")
                 .setProfileName("Test User");
 
         String expectedUserName = "test_user";
+        String expectedEmailId = "test@gmail.com";
+        String expectedProfileName = "Test User";
 
-        UserDTO actualUserDTO = userService.createUser(userDTO);
-        assertEquals(actualUserDTO.getUserName(), expectedUserName);
-    }
+        doReturn(Optional.of(user))
+                .when(usersRepository)
+                .findById(12345l);
 
-    @Test
-    public void fetchUserFromId_success() {
+        Optional<UserDTO> optionalUserDTO = userService.getUserFromId(12345l);
+        assertEquals(expectedUserName, optionalUserDTO.get().getUserName());
+        assertEquals(expectedEmailId, optionalUserDTO.get().getEmailId());
+        assertEquals(expectedProfileName, optionalUserDTO.get().getProfileName());
     }
 
 }
